4a17384adae62ef1af928bd4dba8884a824009fe
taisei0107-cmd <taisei.120107@icloud.com>
2020-07-01 15:54:20 +0900

    課題１(コンパイラしてない)

--- k01/k01.c
+++ k01/k01.c
@@ -3,12 +3,17 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave)
-extern double var_online()
+extern double ave_online(int N,double val,double ave)
+extern double var_online(int N,double val,double ave,double square_ave)
+double val;
+double ave;
+double var;
+double square_ave;
+int N;
+double variarance;
 
 int main(void)
 {
-    double val;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
@@ -26,11 +31,9 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
-
+        ave = ave_online(N,val,ave);
+        var = var_online(N,val,ave,square_ave);
     
-
-
 
     }
 
@@ -38,10 +41,23 @@ int main(void)
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
     }
-
+    variarance = ((N-1)/N)*var;
+    printf("sample mean:%lf\n",ave);
+    printf("sample variarance:%lf\n",var);
+    printf("population mean:%lf\n",ave);
+    printf("population variarance:%lf\n",variarance);
 
     return 0;
 
 
 }
 
+extern double ave_online(int N,double val,double ave)
+{
+    return ((N-1)/N)*ave+(1/N)*val;
+}
+
+extern double var_online(int N,double val,double ave,double square_ave)
+{
+    return (((N-1)/N)*square_ave+(1/N)*ave)-(((N-1)/N)*ave+(1/N)*val);
+}
